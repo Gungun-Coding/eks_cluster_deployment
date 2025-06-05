345146cb4034663123dc15d663bd38528b1f4767
coding star <pradipkakade321@gmail.com>
2025-06-05 21:04:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,11 @@ resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.ankit-cluster.name
   node_group_name = "pc-node-group"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids      = data.aws_subnets.available-subnets.ids
+  #subnet_ids      = data.aws_subnets.available-subnets.ids
+  subnet_ids = [
+    "subnet-064060b6947c62a54",
+    "subnet-07f58f491f381fd9a"
+  ]
   capacity_type   = "ON_DEMAND"
   disk_size       = "20"
   instance_types  = ["t2.micro"]
